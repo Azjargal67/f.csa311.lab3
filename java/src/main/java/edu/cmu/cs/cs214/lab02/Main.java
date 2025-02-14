f9bb7ead8e4a9160419e08ee164238ba839dda40
Azjargal67 <azjargal1627@gmail.com>
2025-02-14 15:40:37 +0800

    lab3 done

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,23 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(4);
+        Shape circle = new Circle(5);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer rectangleRenderer = new Renderer(rectangle);
+        rectangleRenderer.draw();
 
-        renderer.draw();
+        Renderer squareRenderer = new Renderer(square);
+        squareRenderer.draw();
+
+        Renderer circleRenderer = new Renderer(circle);
+        circleRenderer.draw();
     }
 }
